6093a90767378e3b7d89552e0dcea476895303b7
Robert Gordon <Komeuppance@gmail.com>
2020-02-13 00:21:23 -0800

    Adjusted URI

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Launch from './components/Launch.js';
 import './App.css';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql'
+  uri: '/graphql'
 });
 
 function App() {
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
