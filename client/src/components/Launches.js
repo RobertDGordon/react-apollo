41bf29f37bb6d8bbd6e2e03279596baa26a4cf73
Robert Gordon <Komeuppance@gmail.com>
2020-02-12 23:25:22 -0800

    Rendering launch data

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -18,19 +18,25 @@ const LAUNCHES_QUERY = gql`
 export class Launches extends Component {
     render() {
         return (
-            <div>
+            <>
                 <h1 className='display-4 my-3'>Launches</h1>
                 <Query query={LAUNCHES_QUERY}>
                     {
                         ({ loading, error, data }) => {
                             if(loading) return <h4>Loading...</h4>
                             if(error) console.log(error);
-                            console.log(data)
-                            return<h1>test</h1>
+                            // console.log(data)
+                            return<>
+                                {
+                                    data.launches.map(launch => (
+                                        <LaunchItem key={launch.flight_number} launch={launch} />
+                                    ))
+                                }
+                            </>
                         }
                     }
                 </Query>
-            </div>
+            </>
         )
     }
 }
