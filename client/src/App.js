3079b062e99c643e0ec7a1d219bcf4b5799c6a63
Robert Gordon <Komeuppance@gmail.com>
2020-02-12 23:05:33 -0800

    Added Launches.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import './App.css';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
 
 function App() {
   return (
-    <div className="App">
+    <ApolloProvider client={client}>
+    <div className="container">
       <h1>SpaceX</h1>
     </div>
+    </ApolloProvider>
   );
 }
 
