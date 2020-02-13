9f5fd8f3471f9772c29d24dcf871b94f244a6880
Robert Gordon <Komeuppance@gmail.com>
2020-02-13 00:13:21 -0800

    Ready for deploy

---
+++ public/precache-manifest.9eda91989dfb5cbe2060d9a6e36d2e31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a0444a50ae87e39ce92da6c56007d2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90fe55cfaec7e75c850b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "834af05fccce6d90bc27",
+    "url": "/static/js/2.16c7b608.chunk.js"
+  },
+  {
+    "revision": "a0c18f8786874a74d6fd4d46260562f6",
+    "url": "/static/js/2.16c7b608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90fe55cfaec7e75c850b",
+    "url": "/static/js/main.3374668d.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
